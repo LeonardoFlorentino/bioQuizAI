4a638c13b42cb0a3ab4afe2ba5c2381cbefbfe5f
LeonardoFernandes <leonardo.ff.21@gmail.com>
2026-03-23 22:59:15 -0300

    feat: add multiple questions, score and result screen

--- src/app/quiz/quiz.ts
+++ src/app/quiz/quiz.ts
@@ -8,22 +8,68 @@ import { CommonModule } from '@angular/common';
   styleUrl: './quiz.css',
 })
 export class Quiz {
-  question = 'Qual é a organela responsável pela respiração celular?';
+  questions = [
+    {
+      question: 'Qual é a organela responsável pela respiração celular?',
+      options: ['Mitocôndria', 'Núcleo', 'Ribossomo', 'Lisossomo'],
+      correctAnswer: 'Mitocôndria',
+    },
+    {
+      question: 'Qual molécula carrega a informação genética?',
+      options: ['RNA', 'Proteína', 'DNA', 'Lípidio'],
+      correctAnswer: 'DNA',
+    },
+    {
+      question: 'Qual processo as plantas usam para produzir energia?',
+      options: ['Respiração', 'Fermentação', 'Fotossíntese', 'Digestão'],
+      correctAnswer: 'Fotossíntese',
+    },
+  ];
 
-  options = ['Mitocôndria', 'Núcleo', 'Ribossomo', 'Lisossomo'];
-
-  correctAnswer = 'Mitocôndria';
-
+  currentQuestionIndex = 0;
+  score = 0;
   selectedAnswer: string | null = null;
   result: string | null = null;
+  showNext = false;
+  finished = false;
+
+  get currentQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
   selectAnswer(option: string) {
+    if (this.selectedAnswer) return;
+
     this.selectedAnswer = option;
 
-    if (option === this.correctAnswer) {
-      this.result = '✅ Resposta correta!';
+    if (option === this.currentQuestion.correctAnswer) {
+      this.score++;
+      this.result = '✅ Correto!!';
     } else {
-      this.result = '❌Resposta incorreta';
+      this.result = `❌ Errado!"`;
     }
+
+    this.showNext = true;
+  }
+
+  nextQuestion() {
+    this.selectedAnswer = null;
+    this.result = null;
+    this.showNext = false;
+
+    if (this.currentQuestionIndex + 1 < this.questions.length) {
+      this.currentQuestionIndex++;
+    } else {
+      this.finished = true;
+    }
+  }
+
+  restart() {
+    this.currentQuestionIndex = 0;
+    this.score = 0;
+    this.selectedAnswer = null;
+    this.result = null;
+    this.showNext = false;
+    this.finished = false;
   }
 }
